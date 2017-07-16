0d4c56d951b0e9796ad869dc3c5f462954c60d5c
Enoch2k2 <enoch2k2@hotmail.com>
2017-07-16 18:17:04 -0500

    updated canvas to be created in javascript and added title menu

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,14 +1,32 @@
-var canvas, ctx;
+var canvas, ctx, startMenuFlag;
 
 const HEIGHT = 600;
 const WIDTH = 800;
+
 function initializeGame(){
-  // grab canvas
-  canvas = document.querySelector('canvas');
+  // create canvas
+  canvas = document.createElement('canvas');
+  canvas.style.display = "block";
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+  canvas.style.marginLeft = "auto";
+  canvas.style.marginRight = "auto";
+  debugger;
+  // display canvas
+  game = document.querySelector('.game');
+  game.appendChild(canvas);
   ctx = canvas.getContext('2d');
 
-  debugger;
-  // set start point
+  // set canvas start style
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+  // create menu for starting out
+  TitleScreen();
+}
+
+function TitleScreen(){
+  ctx.fillStyle = "white";
+  ctx.font="24px Arial";
+  ctx.fillText("New Game", (WIDTH/2 - 60),(HEIGHT/2 - 50));
 }
